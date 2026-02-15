328c0151c8f148a92c830eff80679dedf7dcae81
Abdelrahman-El-Gendy <abdelrahmanelgendy961@gmail.com>
2026-02-16 00:40:46 +0200

    feat: Introduce PreferencesManager interface
    Adds a new `PreferencesManager` interface to the `core/common` module to abstract interactions with persistent storage.
    
    This interface defines methods for getting and setting `String` and `Boolean` values, providing a standardized way to manage user preferences and other stored data. It also includes `Flow` properties for observing theme and language changes reactively.

---
+++ core/common/src/commonMain/kotlin/com/merchpulse/core/common/PreferencesManager.kt
@@ -0,0 +1,14 @@
+package com.merchpulse.core.common
+
+import kotlinx.coroutines.flow.Flow
+
+interface PreferencesManager {
+    fun getString(key: String, defaultValue: String): String
+    fun setString(key: String, value: String)
+    fun getBoolean(key: String, defaultValue: Boolean): Boolean
+    fun setBoolean(key: String, value: Boolean)
+    
+    // For flow-based updates if needed later
+    val themeStream: Flow<String>
+    val languageStream: Flow<String>
+}
